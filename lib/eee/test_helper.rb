33e1edb99905d86d78b8f05ef77f4f2e4a178930
Filip Zrůst <filip.zrust@zonio.net>
2013-04-28 23:14:59 +0200

    TestHelper is a module again
    Cucumber requires that its World objects is initialized with modules,
    so our module implements eee method which returns Config class with
    practically the same method as there were before but without eee_
    prefix in their names.

--- lib/eee/test_helper.rb
+++ lib/eee/test_helper.rb
@@ -2,39 +2,47 @@ require 'eee'
 
 module EEE
 
-  class TestHelper
+  module TestHelper
 
-    def eee_server(index = 0)
-      Scenario.new Methods::Client.new, config['servers'][index]
+    class Config
+
+      def server(index = 0)
+        Scenario.new Methods::Client.new, config['servers'][index]
+      end
+
+      def user(name = nil, domain = 0, server = 0)
+        name = config['default_user'] if name.nil?
+
+        user_config = config['users'][name]
+        server_config = config['servers'][name]
+        user_config['username'] =
+          "#{name.downcase}@#{server_config['domains'][domain]}"
+
+        Entities::User[user_config]
+      end
+
+      def user_password(name = nil)
+        name = config['default_user'] if name.nil?
+
+        config['users'][name]['password']
+      end
+
+      private
+
+      def config
+        YAML.load_file @config
+      end
+
+      public
+
+      def initialize(config_filename)
+        @config = config_filename
+      end
+
     end
 
-    def eee_user(name = nil, domain = 0, server = 0)
-      name = config['default_user'] if name.nil?
-
-      user_config = config['users'][name]
-      server_config = config['servers'][name]
-      user_config['username'] =
-        "#{name.downcase}@#{server_config['domains'][domain]}"
-
-      Entities::User[user_config]
-    end
-
-    def eee_user_password(name = nil)
-      name = config['default_user'] if name.nil?
-
-      config['users'][name]['password']
-    end
-
-    private
-
-    def config
-      YAML.load_file @config
-    end
-
-    public
-
-    def initialize(config_filename)
-      @config = config_filename
+    def eee(config_filename)
+      Config.new config_filename
     end
 
   end
